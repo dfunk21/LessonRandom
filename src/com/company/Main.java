48ead2fb7b41147ca3103a482c3f07dc752a9770
df377 <df377@pvsd.org>
2016-11-29 14:30:28 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+import java.util.Random;
+public class Main {
+
+    public static void main(String[] args) {
+
+        Random rd = new Random();
+        //nextInt()
+            //int x = (rd.nextInt(100)) + 1
+                //Gives random numbers from 1-100
+        //nextDouble()
+            //returns between 0-1 with doubles
+    }
+}
